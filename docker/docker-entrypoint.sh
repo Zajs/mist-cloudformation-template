0621197f5ae9fa963677d2d3af4a520ba19b7d63
zajs <aga.zajs@gmail.com>
2017-05-28 00:37:50 +0300

    Changed configuration

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,10 +4,12 @@ set -e
 cd ${MIST_HOME}
 
 if [ "$1" = 'master' ]; then
-  ./bin/mist start master --config $MIST_CONFIG
+  $IP=$(cat /etc/hostname)
+  ./bin/mist start master --config $MIST_CONFIG --java-args "-Dmist.akka.remote.netty.tcp.hostname=$IP -Dmist.akka.remote.netty.tcp.port=2551"
 elif [ "$1" = 'worker' ]; then
   $SPARK_HOME/sbin/start-master.sh -h 0.0.0.0
-  ./bin/mist start worker --runner local --namespace $NAMESPACE --config $MIST_CONFIG $RUN_OPTIONS
+  $IP=$(cat /etc/hostname)
+  ./bin/mist start worker --runner local --namespace $NAMESPACE --java-args "-Dmist.akka.remote.netty.tcp.hostname=$IP -Dmist.akka.remote.netty.tcp.port=2551" --config $MIST_CONFIG $RUN_OPTIONS
 elif [ "$1" = 'spark-slave' ]; then
   $SPARK_HOME/sbin/start-slave.sh $2
   PID=`cat /tmp/spark-*.pid`
