ece696c1afe21a9377e8e6ba25b292c0430f1c1a
zajs <aga.zajs@gmail.com>
2017-05-28 02:51:32 +0300

    Fixed spark slave entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -14,11 +14,12 @@ elif [ "$1" = 'spark-slave' ]; then
   $SPARK_HOME/sbin/start-slave.sh $2
   PID=`cat /tmp/spark-*.pid`
   echo "Started PID=$PID"
-  while ps -p $PID > /dev/null
+  while [ -e /proc/$PID ]
   do
-    sleep 60
+    sleep 10
   done
   echo "Finished"
+  ps aux
 else
   exec "$@"
 fi
